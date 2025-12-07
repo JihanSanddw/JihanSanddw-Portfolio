eaf3246a5570ee3ebeca4a9fa67def51c1131655
JihanSanddw <jihansanddw@gmail.com>
2025-12-07 20:14:38 +0700

    update 8

--- src/main.js
+++ src/main.js
@@ -1,20 +1,21 @@
+document.getElementById("btn").addEventListener("click", () => {
+    setTimeout(() => {
+        document.getElementById("inputNama").value = "";
+        document.getElementById("inputEmail").value = "";
+        document.getElementById("sendMessage").value = "";
+    }, 5);
+})
+
 function showNavbar() {
     const navbar = document.getElementById('navbar');
-    // Menampilkan navbar (mengubah display menjadi 'flex' atau 'block' sesuai CSS)
     navbar.style.display = 'flex'; 
-    // Jika Anda menggunakan transisi, Anda mungkin perlu menambahkan class tertentu di sini
-    // navbar.classList.add('active');
 }
 
 function hideNavbar() {
     const navbar = document.getElementById('navbar');
-    // Menyembunyikan navbar
     navbar.style.display = 'none';
-    // Jika Anda menggunakan transisi, Anda mungkin perlu menghapus class tertentu di sini
-    // navbar.classList.remove('active');
 }
 
-// Opsional: Jika Anda ingin menggunakan satu fungsi untuk menampilkan/menyembunyikan
 function toggleNavbar() {
     const navbar = document.getElementById('navbar');
     
