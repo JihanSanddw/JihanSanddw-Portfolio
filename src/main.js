2d7fb911e4095c71f974ce9c64f6f2c495cca8e2
JihanSanddw <jihansanddw@gmail.com>
2026-01-30 20:22:00 +0700

    Update-11

--- src/main.js
+++ src/main.js
@@ -5,24 +5,3 @@ document.getElementById("btn").addEventListener("click", () => {
         document.getElementById("sendMessage").value = "";
     }, 5);
 })
-
-function showNavbar() {
-    const navbar = document.getElementById('navbar');
-    navbar.style.display = 'flex'; 
-}
-
-function hideNavbar() {
-    const navbar = document.getElementById('navbar');
-    navbar.style.display = 'none';
-}
-
-function toggleNavbar() {
-    const navbar = document.getElementById('navbar');
-    
-    // Logika untuk beralih (toggle) antara 'flex' dan 'none'
-    if (navbar.style.display === 'flex') {
-        navbar.style.display = 'none';
-    } else {
-        navbar.style.display = 'flex';
-    }
-}
